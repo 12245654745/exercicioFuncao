4ac622c3292697c7f28c29c5b080c117bc44fa0d
12245654745 <152646306+12245654745@users.noreply.github.com>
2024-04-05 19:38:58 +0000

    vitoria

---
+++ atividade2.js
@@ -0,0 +1,11 @@
+function PersonagemMerreu(dano, saude){
+
+    const personagem = saude - dano
+if(personagem <= 0){
+    console.log(1)
+}else {
+    console.log(0)
+}
+
+}
+PersonagemMerreu(100, 20)
